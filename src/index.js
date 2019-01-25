bd53c5e070c019055c17b178f768346ad7a5da14
Yariv Gilad aka Ajar Bahamonde <yariv.gilad@gmail.com>
2019-01-25 23:01:59 +0200

    fix: added a filename rule

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ function get_filename(){
             (line && line.includes('internal/modules/cjs')) || 
             (line.includes('<anonymous>')) ||
             (line.includes('_stream_writable.js')) ||
+            (line.includes('next_tick.js')) ||
             (line && line.includes('events.js'))) line = lines[3];
         if(!line) return '>>';
         const res = line.match(/([\w\d\-_.]*:\d+:\d+)/);
