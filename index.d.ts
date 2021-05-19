27b2b50f7532539f1fa59bde62646f85fa52bbbd
Yariv Gilad <yariv.gilad@gmail.com>
2021-05-19 22:21:15 +0300

    fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,67 +1,66 @@
-
-
-type LogFunc = (...args: any[])=> string;
-type ErrorLogFunc = (err: Error)=> {msg:string,stack:string[]};
-interface Obj{
-    [key: string]:any
-}
-type ObjectLogFunc = (obj:Obj,label?:string) => {actual_label:string};
-
-interface Constants{
-    Reset:string;
-    Bold:string;
-    Dim:string;
-    Underscore:string;
-    Blink:string; 
-    Reverse:string;
-    Hidden:string;
-    //Colors
-    NoColor:string;
-    Black:string;
-    Red:string;
-    Green:string;
-    Yellow:string;
-    Blue:string;
-    Magenta:string;
-    Cyan:string;
-    White:string;
-    //Backgrounds
-    BgBlack:string;
-    BgRed:string;
-    BgGreen:string;
-    BgYellow:string;
-    BgBlue:string;
-    BgMagenta:string;
-    BgCyan:string;
-    BgWhite:string;
-}
-
-interface Marker{
-    //by levels shorthand
-    n :         LogFunc,    
-    v :         LogFunc,    
-    d :         LogFunc,       
-    i :         LogFunc,      
-    w :         LogFunc,     
-    e :         LogFunc,       
-    //by explicit levels  
-    none:       LogFunc,    
-    verbose:    LogFunc,    
-    debug:      LogFunc,       
-    info:       LogFunc,      
-    warn:       LogFunc,     
-    err:        LogFunc,       
-    //by color names
-    red:        LogFunc,       
-    green:      LogFunc,     
-    blue:       LogFunc,      
-    cyan:       LogFunc,      
-    yellow:     LogFunc,  
-    magenta:    LogFunc,   
+declare module '@ajar/marker'{
     
+    type LogFunc = (...args: any[])=> string;
+    type ErrorLogFunc = (err: Error)=> {msg:string,stack:string[]};
+    interface Obj{
+        [key: string]:any
+    }
+    type ObjectLogFunc = (obj:Obj,label?:string) => {actual_label:string};
+
+    interface Constants{
+        Reset:string;
+        Bold:string;
+        Dim:string;
+        Underscore:string;
+        Blink:string; 
+        Reverse:string;
+        Hidden:string;
+        //Colors
+        NoColor:string;
+        Black:string;
+        Red:string;
+        Green:string;
+        Yellow:string;
+        Blue:string;
+        Magenta:string;
+        Cyan:string;
+        White:string;
+        //Backgrounds
+        BgBlack:string;
+        BgRed:string;
+        BgGreen:string;
+        BgYellow:string;
+        BgBlue:string;
+        BgMagenta:string;
+        BgCyan:string;
+        BgWhite:string;
+    }
+
+    //by levels shorthand
+    export const n :         LogFunc;    
+    export const v :         LogFunc;    
+    export const d :         LogFunc;       
+    export const i :         LogFunc;      
+    export const w :         LogFunc;     
+    export const e :         LogFunc;       
+    //by explicit levels  
+    export const none:       LogFunc;    
+    export const verbose:    LogFunc;    
+    export const debug:      LogFunc;       
+    export const info:       LogFunc;      
+    export const warn:       LogFunc;     
+    export const err:        LogFunc;       
+    //by color names
+    export const red:        LogFunc;       
+    export const green:      LogFunc;     
+    export const blue:       LogFunc;      
+    export const cyan:       LogFunc;      
+    export const yellow:     LogFunc;  
+    export const magenta:    LogFunc;   
+        
     //some extras
-    obj:        ObjectLogFunc;
-    error:      ErrorLogFunc;
-    constants:  Constants;  
+    export const obj:        ObjectLogFunc;
+    export const error:      ErrorLogFunc;
+    export const constants:  Constants;  
+    
 }
-export default Marker;
